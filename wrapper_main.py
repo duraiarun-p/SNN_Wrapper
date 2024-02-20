9b70e1d2379c009f1aea0321a39d853eb8fababc
durairun-p <arun.durai.p@gmail.com>
2024-02-20 12:12:52 +0000

    Template commit

--- wrapper_main.py
+++ wrapper_main.py
@@ -1,7 +1,28 @@
 # Python Script created using PyCharm IDE
 # Created by pxb23215 at 20/02/2024
 ###############################################
+# %% Import Libraries
 
 import sys
 
 print('Python %s on %s' % (sys.version, sys.platform))
+
+# %% Dataset downloader
+
+
+# %% Dataloader (Training and test data)
+
+
+# %% Model specification and generation
+
+
+# %% Training operation (Scratch Training or Update-based Training)
+
+
+# %% Testing operation
+
+
+# %% Model saving
+
+
+# %% Visualisation
