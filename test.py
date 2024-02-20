552f6cb047a594045f029257730e0691a9d5a43f
durairun-p <arun.durai.p@gmail.com>
2024-02-20 11:00:15 +0000

    First commit

---
+++ test.py
@@ -0,0 +1,2 @@
+# Python Script created using PyCharm IDE
+# Created by pxb23215 at 20/02/2024
---
+++ wrapper_main.py
@@ -0,0 +1,7 @@
+# Python Script created using PyCharm IDE
+# Created by pxb23215 at 20/02/2024
+###############################################
+
+import sys
+
+print('Python %s on %s' % (sys.version, sys.platform))
